4be46ebcde04c5b080361473e98ef23a5b247687
ALWIN <alwinedgar13@gmail.com>
2023-04-14 22:41:04 -0500

    made 3 buttons for the main menu and made a UpdateMonthlyIncome method

--- app/src/main/java/edu/utsa/cs3443/project/income.java
+++ app/src/main/java/edu/utsa/cs3443/project/income.java
@@ -1,7 +1,10 @@
 package edu.utsa.cs3443.project;
+import androidx.annotation.NonNull;
+
+import java.util.Scanner;
 //income.java will take user input of annual and monthly income
 //getters and setters for monthly and annual income
-//toString method will concat. the
+//toString method will concat. both of the incomes
 public class income {
 
     double monthlyIncome;
@@ -23,10 +26,29 @@ public class income {
         this.annualIncome = annualIncome;
     }
 
+    @NonNull
     @Override
     public String toString() {
         return  "Monthly Income= $" + monthlyIncome + "\n Annual Income= $" + annualIncome;
     }
+
+    /**
+     * UpdateIncome method takes user input and allows
+     * user to change their monthly income
+     */
+    private void UpdateMonthlyIncome() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter new monthly income: ");
+        double newMonthlyIncome = sc.nextDouble();
+        this.monthlyIncome = newMonthlyIncome;
+        System.out.println("Your monthly income has been updated to: $" + newMonthlyIncome);
+        //double newYearlyIncome = newMonthlyIncome * 12;
+
+
+    }
+    //if user chooses to update monthly income call UpdateMonthlyIncome
+
 
     //here, logic to communicate with screen 2 methods
     //methods to add and deduct monthly income based on expenses and recurring expenses from screen 2
