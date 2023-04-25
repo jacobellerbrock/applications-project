147642e7c15afec810b88ff1643a2b6f55f48e79
Rachel <easton.rachel03@gmail.com>
2023-04-24 19:39:23 -0500

    all model classes are added (not tested yet)

--- app/src/main/java/edu/utsa/cs3443/project/CategoryActivity.java
+++ app/src/main/java/edu/utsa/cs3443/project/CategoryActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PaymentActivity extends AppCompatActivity {
+public class CategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
