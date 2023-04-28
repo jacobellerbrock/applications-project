a3a065f3e41eb9b3b47fdc2dd11acf9790535647
Jacob Ellerbrock <ja.eller14@gmail.com>
2023-04-28 13:30:27 -0500

    finished CategoryActivity and CategoryController, should be easy copy to the other controller TODO: Incorporate what to do with the data (talk to Rachel about what the functions do)

--- app/src/main/java/edu/utsa/cs3443/project/controller/CategoryController.java
+++ app/src/main/java/edu/utsa/cs3443/project/controller/CategoryController.java
@@ -2,20 +2,34 @@ package edu.utsa.cs3443.project.controller;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
 
 import edu.utsa.cs3443.project.CategoryActivity;
 
 public class CategoryController implements View.OnClickListener {
 
     CategoryActivity p;
+    double rent = 0, car = 0, card = 0, food = 0, savings = 0;
+    
+    public CategoryController(double rent, double car, double card, double food, double savings) {
+        this.rent = rent;
+        this.car = car;
+        this.card = card;
+        this.food = food;
+        this.savings = savings;
+    }
+    
     /**
      * @param v The view that was clicked.
      */
+    
+    
     @Override
     public void onClick(View v) {
         Intent intent = new Intent(p, CategoryActivity.class);
-        intent.putExtra("buttonId", v.getId());
         p.startActivity(intent);
+    
+        // do things with functions and given data
 
     }
 }
--- app/src/main/java/edu/utsa/cs3443/project/controller/MainController.java
+++ app/src/main/java/edu/utsa/cs3443/project/controller/MainController.java
@@ -3,20 +3,38 @@ package edu.utsa.cs3443.project.controller;
 import android.content.Intent;
 import android.view.View;
 
+import edu.utsa.cs3443.project.CategoryActivity;
+import edu.utsa.cs3443.project.DataActivity;
+import edu.utsa.cs3443.project.IncomeActivity;
 import edu.utsa.cs3443.project.MainActivity;
+import edu.utsa.cs3443.project.R;
 
 public class MainController implements View.OnClickListener {
-    MainActivity main;
-
+    MainActivity mainActivity;
+    
+    public MainController(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+    
     /**
      * @param v The view that was clicked.
      */
+    
+    
 
     public void onClick(View v) {
-        Intent intent = new Intent(main, MainActivity.class);
-        intent.putExtra("buttonId", v.getId());
-        main.startActivity(intent);
-
-
+        Intent intent = null;
+        switch(v.getId()) {
+            case R.id.Paymentbutton:
+                intent = new Intent(mainActivity, CategoryActivity.class);
+                break;
+            case R.id.IncomeButton:
+                intent = new Intent(mainActivity, IncomeActivity.class);
+                break;
+            case R.id.BudgetButton:
+                intent = new Intent(mainActivity, DataActivity.class);
+        }
+        assert intent != null;
+        mainActivity.startActivityForResult(intent, MainActivity.REQUEST_CODE);
     }
 }
