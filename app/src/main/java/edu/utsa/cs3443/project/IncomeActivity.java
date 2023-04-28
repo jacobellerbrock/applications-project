f9652536f635974797b3f8c556684bfe68e4fc40
Jacob Ellerbrock <ja.eller14@gmail.com>
2023-04-28 16:23:21 -0500

    Finished Category and Income portion

--- app/src/main/java/edu/utsa/cs3443/project/IncomeActivity.java
+++ app/src/main/java/edu/utsa/cs3443/project/IncomeActivity.java
@@ -3,6 +3,14 @@ package edu.utsa.cs3443.project;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.Scanner;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+
+import edu.utsa.cs3443.project.controller.BackButtonController;
+import edu.utsa.cs3443.project.controller.CategoryController;
+import edu.utsa.cs3443.project.controller.IncomeController;
+import edu.utsa.cs3443.project.controller.SwitchController;
 
 
 public class IncomeActivity extends AppCompatActivity {
@@ -11,11 +19,18 @@ public class IncomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_income);
-
-        //if user chooses to update monthly income call UpdateMonthlyIncome or put UpdateMonthlyIncome here
-        //here, logic to communicate with screen 2 methods
-        //methods to add and deduct monthly income based on expenses and recurring expenses from screen 2
-
-
+    
+        Button menuButton = findViewById(R.id.backtomenu_button);
+        BackButtonController backButtonController = new BackButtonController(this);
+        menuButton.setOnClickListener(backButtonController);
+    
+        Switch monthYearSwitch = findViewById(R.id.monthYearSwitch);
+        SwitchController switchController = new SwitchController(monthYearSwitch);
+        monthYearSwitch.setOnClickListener(switchController);
+        EditText incomeText = findViewById(R.id.input_income);
+    
+        Button calculateButton = findViewById(R.id.calculate_button);
+        IncomeController incomeController = new IncomeController(monthYearSwitch, incomeText, this);
+        calculateButton.setOnClickListener(incomeController);
     }
 }
--- app/src/main/java/edu/utsa/cs3443/project/controller/IncomeController.java
+++ app/src/main/java/edu/utsa/cs3443/project/controller/IncomeController.java
@@ -1,21 +1,33 @@
 package edu.utsa.cs3443.project.controller;
 
-import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import edu.utsa.cs3443.project.IncomeActivity;
 
 public class IncomeController implements View.OnClickListener {
-
-    IncomeActivity income;
-    /**
-     * @param v The view that was clicked.
-     */
+    
+    private IncomeActivity incomeActivity;
+    private Switch monthYearSwitch;
+    private EditText incomeText;
+    private static double income = 0;
+    enum Time {MONTHLY, YEARLY};
+    private static Time time;
+    
+    public IncomeController(Switch monthYearSwitch, EditText incomeText, IncomeActivity incomeActivity) {
+        this.incomeActivity = incomeActivity;
+        this.monthYearSwitch = monthYearSwitch;
+        this.incomeText = incomeText;
+    }
+    
     @Override
-    public void onClick(View v) {
-        Intent intent = new Intent(income, IncomeActivity.class);
-        intent.putExtra("buttonid", v.getId());
-        income.startActivity(intent);
-
+    public void onClick(View view) {
+        income = (incomeText.getText().toString().equals("")) ? 0 : Double.parseDouble(incomeText.getText().toString());
+        time = (monthYearSwitch.isChecked()) ? Time.YEARLY : Time.MONTHLY;
+        
+        Toast t = Toast.makeText(view.getContext(), "Income added to budget", Toast.LENGTH_SHORT);
+        t.show();
     }
 }
